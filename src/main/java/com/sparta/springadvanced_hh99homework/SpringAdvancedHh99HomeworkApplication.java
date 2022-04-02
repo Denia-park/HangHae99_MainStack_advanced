361442842e12fbdf3f969e1ae353f6fed67122ab
DESKTOP-O3U018O\Hyunggi <phg9587@naver.com>
2022-04-02 22:45:39 +0900

    - Main 파일 추가

---
+++ src/main/java/com/sparta/springadvanced_hh99homework/SpringAdvancedHh99HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springadvanced_hh99homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedHh99HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedHh99HomeworkApplication.class, args);
+    }
+
+}
