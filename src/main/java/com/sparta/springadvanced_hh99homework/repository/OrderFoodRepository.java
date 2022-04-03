b557f36de8b2f03366b3c27bc6a3767e31b821a5
DESKTOP-O3U018O\Hyunggi <phg9587@naver.com>
2022-04-04 00:49:40 +0900

    OrderRepo 추가

---
+++ src/main/java/com/sparta/springadvanced_hh99homework/repository/OrderFoodRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.springadvanced_hh99homework.repository;
+
+import com.sparta.springadvanced_hh99homework.model.OrderFood;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderFoodRepository extends JpaRepository<OrderFood,Long> {
+}
