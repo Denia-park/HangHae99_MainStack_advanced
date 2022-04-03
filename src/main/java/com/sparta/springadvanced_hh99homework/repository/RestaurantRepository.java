da6a11d2aa59f6ca182a9cb6fb262a9849cdc5f6
DESKTOP-O3U018O\Hyunggi <phg9587@naver.com>
2022-04-04 00:49:25 +0900

    findById 추가

--- src/main/java/com/sparta/springadvanced_hh99homework/repository/RestaurantRepository.java
+++ src/main/java/com/sparta/springadvanced_hh99homework/repository/RestaurantRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
     Optional<Restaurant> findByName(String name);
+    Optional<Restaurant> findById(Long id);
 
     boolean existsByName(String name);
 }
