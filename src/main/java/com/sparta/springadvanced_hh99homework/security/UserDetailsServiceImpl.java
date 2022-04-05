85afbce8ab1cd00bd6705afa2ed4b1a1a4c7fe41
DESKTOP-O3U018O\Hyunggi <phg9587@naver.com>
2022-04-05 17:09:24 +0900

    스프링 시큐리티 추가

---
+++ src/main/java/com/sparta/springadvanced_hh99homework/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sparta.springadvanced_hh99homework.security;
+
+import com.sparta.springadvanced_hh99homework.model.User;
+import com.sparta.springadvanced_hh99homework.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
